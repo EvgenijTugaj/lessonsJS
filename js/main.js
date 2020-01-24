64fea794819ca9b6911f6d9c73b6e0c708fdbc49
Evgenij Tugaj <79255220650@ya.ru>
2020-01-24 21:39:07 +0300

    Основное задание

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 90000;
+let income = 'Заработок по знакомым';
+let addExpenses = 'комуналка, интернет, бензин, еда';
+let deposit = true;
+let mission = 1000000;
+let period = 12;
 
-alert('Hello, world!');
+console.log('money: ', money);
 
-console.log('Привет, мир!');
+console.log('addExpensesLength: ', addExpenses.length);
+
+console.log(`"Период равен ${period} месяцев" и "Цель заработать ${mission} рублей/долларов/гривен/юани"`);
+
+addExpenses = addExpenses.toLowerCase();
+console.log('addExpenses: ', addExpenses.split(', '));
+
+let budgetDay = money / 30;
+console.log('budgetDay: ', budgetDay);
