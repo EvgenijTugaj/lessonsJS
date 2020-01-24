b7e9ffed625c12d17e21bac2b8c61ff15f42f615
Evgenij Tugaj <79255220650@ya.ru>
2020-01-24 21:33:43 +0300

    Урок 1

--- js/main.js
+++ js/main.js
@@ -1,22 +1,10 @@
-let money = 90000;
-let income = 'Заработок по знакомым';
-let addExpenses = 'комуналка, интернет, бензин, еда';
-let deposit = true;
-let mission = 1000000;
-let period = 12;
+let money;
+let income;
+let addExpenses;
+let deposit;
+let mission;
+let period;
 
-// alert('Hello, world!');
+alert('Hello, world!');
 
-// console.log('Привет, мир!');
-
-console.log('money: ', money);
-
-console.log('addExpensesLength: ', addExpenses.length);
-
-console.log(`"Период равен ${period} месяцев" и "Цель заработать ${mission} рублей/долларов/гривен/юани"`);
-
-addExpenses = addExpenses.toLowerCase();
-console.log('addExpenses: ', addExpenses.split(', '));
-
-let budgetDay = money / 30;
-console.log('budgetDay: ', budgetDay);
+console.log('Привет, мир!');
