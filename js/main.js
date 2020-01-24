14b0e1e1ce16cab8d616cb59b2252b723aca8e1a
Evgenij Tugaj <79255220650@ya.ru>
2020-01-24 22:40:59 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
-let num = 266219;
-let numStr = String(num);
-let multiplier = numStr[0];
+let money;
+let income;
+let addExpenses;
+let deposit;
+let mission;
+let period;
 
+<<<<<<< HEAD
 for(var i = 1; i < numStr.length; i++){
     console.log(multiplier *= +numStr[i]);
 }
 
 multiplier = String(multiplier ** 3); 
 console.log('Число: ', multiplier.substring(0, 2));
+=======
+alert('Hello, world!');
+>>>>>>> parent of 8d06ea6... Усложненное задание второго урока
 
+console.log('Привет, мир!');
