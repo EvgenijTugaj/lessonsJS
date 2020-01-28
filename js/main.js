b456f9d0a7b6210a869b3f009de4f5c3e905961e
Evgenij Tugaj <79255220650@ya.ru>
2020-01-28 22:40:25 +0300

    Основное задание урока 4

--- js/main.js
+++ js/main.js
@@ -8,23 +8,34 @@ let money = parseInt(prompt('Ваш месячный доход?', '200000руб
     mission = 1000000,
     period = 12;
 
-console.log('money: ' + typeof(money) + '\n', 
-'income: ' + typeof(income) + '\n', 'deposit: ' + typeof(deposit) + '\n');
-console.log('addExpensesLength: ', addExpenses.length);
-console.log(`"Период равен ${period} месяцев" и "Цель заработать ${mission} рублей/долларов/гривен/юани"`);
-console.log('addExpenses: ', addExpenses.toLowerCase().split(', '));
-
 let expenses1 = prompt('Введите обязательную статью расходов?', 'Покупка велосипеда.'),
     amount1 = parseInt(prompt('Во сколько это обойдется?', '10000руб.')),
     expenses2 = prompt('Введите обязательную статью расходов?', 'Поход к хирургу после велосипеда.'),
     amount2 = parseInt(prompt('Во сколько это обойдется?', '20000руб.'));
 
-let budgetMonth = money - amount1 - amount2,
-    budgetDay = Math.floor(budgetMonth / 30);
+function getExpensesMonth(am1, am2){
+    return am1 + am2;
+}
 
+function getAccumulatedMonth(mn, amnt1, amnt2, getExpMon){
+    return mn - getExpMon(amnt1, amnt2);
+}
+
+let accumulatedMonth = getAccumulatedMonth(money, amount1, amount2, getExpensesMonth);
+
+function getTargetMonth(allMoney, acmon){
+    return Math.ceil(allMoney / acmon);
+}
+
+let budgetDay = Math.floor(accumulatedMonth / 30);
+
+console.log('money: ' + typeof(money) + '\n', 
+'income: ' + typeof(income) + '\n', 'deposit: ' + typeof(deposit) + '\n');
+console.log(`"Период равен ${period} месяцев" и "Цель заработать ${mission} рублей/долларов/гривен/юани"`);
+console.log('Вывод возможных расходов: ', addExpenses.toLowerCase().split(', '));
 console.log('Ваш месячный доход: ', money);
-console.log('Бюджет за месяц:', budgetMonth);
-console.log('Цель будет достигнута за:', Math.ceil(mission / budgetMonth), 'месяцев');
+console.log('Бюджет за месяц:', accumulatedMonth);
+console.log('Цель будет достигнута за:', getTargetMonth(mission, accumulatedMonth), 'месяцев');
 console.log('Бюджет на день:', budgetDay);
  
 switch(true){
@@ -40,3 +51,4 @@ switch(true){
     default:
         console.log('Что то пошло не так');
 }
+
